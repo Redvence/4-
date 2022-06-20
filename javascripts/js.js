fe0acc05b74978b76a2e05d7aab25833129a80bd
Vanurina Masha <90695332+Redvence@users.noreply.github.com>
2022-06-20 22:19:44 +0300

    //

---
+++ javascripts/js.js
@@ -0,0 +1,59 @@
+$(function(){
+  $(".txt1").hover(function(){
+    $(".main1bg").css("opacity","1");
+  }, function(){
+    $(".main1bg").css("opacity","0");
+  });
+});
+$(function(){
+  $(".txt2").hover(function(){
+    $(".main2bg").css("opacity","1");
+  }, function(){
+    $(".main2bg").css("opacity","0");
+  });
+});
+$(function(){
+  $(".txt3").hover(function(){
+    $(".main3bg").css("opacity","1");
+  }, function(){
+    $(".main3bg").css("opacity","0");
+  });
+});
+$(function(){
+  $(".txt4").hover(function(){
+    $(".main4bg").css("opacity","1");
+  }, function(){
+    $(".main4bg").css("opacity","0");
+  });
+});
+$(function(){
+  $(".txt5").hover(function(){
+    $(".main5bg").css("opacity","1");
+  }, function(){
+    $(".main5bg").css("opacity","0");
+  });
+});
+$(function(){
+  $(".txt6").hover(function(){
+    $(".main6bg").css("opacity","1");
+  }, function(){
+    $(".main6bg").css("opacity","0");
+  });
+});
+$(function(){
+  $(".txt7").hover(function(){
+    $(".main7bg").css("opacity","1");
+  }, function(){
+    $(".main7bg").css("opacity","0");
+  });
+});
+$(function(){
+  $(".logo").hover(function(){
+    $(".main1bg,.main2bg,.main3bg,.main4bg,.main5bg,.main6bg,.main7bg").css("opacity","1");
+  }, function(){
+    $(".main1bg,.main2bg,.main3bg,.main4bg,.main5bg,.main6bg,.main7bg").css("opacity","0");
+  });
+});
+$(function() {
+	$('.kek').draggable();
+});
